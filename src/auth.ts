53b8dbee72d2d1a49df1d0146b0e643023e846e4
Giao Le <giolynx104@gmail.com>
2024-10-09 07:35:00 +0700

    feat: Add GitHub as a provider for authentication
    This commit adds the GitHub provider to the authentication configuration in the `auth.ts` file. Now users can sign in using their GitHub accounts in addition to Google. This enhances the authentication options for users and expands the reach of the application.

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,10 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
- 
+import Github from "next-auth/providers/github"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    Google
+    Google,
+    Github
   ],
   pages: {
     signIn: "/signin",
