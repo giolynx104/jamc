065e90e78dd426db5fc22a147283df4e4519bb15
Giao Le <giolynx104@gmail.com>
2024-10-15 10:02:43 +0700

    Add onboarding role to the schema and add prisma adapter for Auth.js

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,16 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
-import Github from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/prisma";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Google,
-    Github
-  ],
+  adapter: PrismaAdapter(prisma),
+  providers: [Google, Github],
   pages: {
     signIn: "/signin",
   },
   session: {
     strategy: "jwt",
   },
-})
+});
